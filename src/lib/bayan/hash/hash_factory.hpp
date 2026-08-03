18b81517cd815b1b5b0ea6ac589b302dec4b6222
Z1nK <llobesa@gmail.com>
2026-08-03 17:15:59 +0200

    feat: implement MakeHashFunction for CRC32 and MD5 hash algorithms

---
+++ src/lib/bayan/hash/hash_factory.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "crc32.hpp"
+#include "hash.hpp"
+#include "md5.hpp"
+
+#include <stdexcept>
+
+namespace bayan {
+
+inline HashFunction MakeHashFunction(HashAlgorithm algorithm) {
+  switch (algorithm) {
+  case HashAlgorithm::CRC32:
+    return
+        [](const char* data, size_t size) -> HashValue { return {crc32(data, size), 0U, 0U, 0U}; };
+  case HashAlgorithm::MD5:
+    return 
+        [](const char* data, size_t size) -> HashValue { return md5(data, size); };
+  }
+
+  throw std::invalid_argument("Unsupported hash algorithm");
+}
+
+}  // namespace bayan
